e920e2fcd86de0ec200cc8a27956f7ce415d78e5
nam pham <nam@vnasoft.com>
2016-01-29 16:20:53 +0700

    Xong trang Life Cycles > ga

--- application/views/frontend/life_cycles_chicken.php
+++ application/views/frontend/life_cycles_chicken.php
@@ -1,24 +1,24 @@
-<div class="aquaticsContainer">
+<div class="chickenContainer">
   <div class="loadingPage">
     <img src="/assets/images/waiting.gif" alt="" />
     <div class="text">
       Loading...
     </div>
   </div>
-  <img class="bg" src="/assets/images/lc-aquatics-bg.jpg" alt="" />
+  <img class="bg" src="/assets/images/lc-chicken-bg.jpg" alt="" />
   <ul class="myEllipse">
+    <a href="<?= site_url('flifecycle/aquatics')?>"><li></li></a>
     <a href=""><li></li></a>
-    <a href="<?= site_url('flifecycle/chicken')?>"><li></li></a>
     <a href="<?= site_url('flifecycle/pig')?>"><li></li></a>
   </ul>
   <div class="content">
-    <div class="title">Aquatics</div>
+    <div class="title">Chicken</div>
     <div class="desc">
-      The life cycle of a:
+      The life cycle of a chicken:
     </div>
     <ul>
-      <li>Shrimp</li>
-      <li>Fish</li>
+      <li>Broiler</li>
+      <li>Layer</li>
     </ul>
   </div>
 </div>
--- assets/js/flifecycles.js
+++ assets/js/flifecycles.js
@@ -10,17 +10,22 @@ jQuery(window).load(function () {
 
   var aquatics = $('.aquaticsContainer');
   lifeCycles.removeClass('small big biggest');
+  var chicken = $('.chickenContainer');
+  lifeCycles.removeClass('small big biggest');
 
   var winWidth = $(window).width();
 
   if(winWidth >= 2738){
     lifeCycles.addClass('biggest');
     aquatics.addClass('biggest');
+    chicken.addClass('biggest');
   }else if (winWidth >= 2282 && winWidth < 2738) {
     lifeCycles.addClass('big');
     aquatics.addClass('big');
+    chicken.addClass('big');
   }else if (winWidth >= 1902 && winWidth < 2282) {}else {
     lifeCycles.addClass('small');
     aquatics.addClass('small');
+    chicken.addClass('small');
   }
 });

256af8c3f1db9b7c07350d1496ffe37926197b2a
nam pham <nam@vnasoft.com>
2016-01-29 16:07:32 +0700

    Xong trang Life Cycles > Aquatics

---
+++ application/views/frontend/life_cycles_aquatics.php
@@ -0,0 +1,24 @@
+<div class="aquaticsContainer">
+  <div class="loadingPage">
+    <img src="/assets/images/waiting.gif" alt="" />
+    <div class="text">
+      Loading...
+    </div>
+  </div>
+  <img class="bg" src="/assets/images/lc-aquatics-bg.jpg" alt="" />
+  <ul class="myEllipse">
+    <a href=""><li></li></a>
+    <a href=""><li></li></a>
+    <a href=""><li></li></a>
+  </ul>
+  <div class="content">
+    <div class="title">Aquatics</div>
+    <div class="desc">
+      The life cycle of a:
+    </div>
+    <ul>
+      <li>Shrimp</li>
+      <li>Fish</li>
+    </ul>
+  </div>
+</div>
--- assets/js/flifecycles.js
+++ assets/js/flifecycles.js
@@ -8,13 +8,19 @@ jQuery(window).load(function () {
   var lifeCycles = $('.lifeCyclesContainer');
   lifeCycles.removeClass('small big biggest');
 
+  var aquatics = $('.aquaticsContainer');
+  lifeCycles.removeClass('small big biggest');
+
   var winWidth = $(window).width();
-  
+
   if(winWidth >= 2738){
     lifeCycles.addClass('biggest');
+    aquatics.addClass('biggest');
   }else if (winWidth >= 2282 && winWidth < 2738) {
     lifeCycles.addClass('big');
+    aquatics.addClass('big');
   }else if (winWidth >= 1902 && winWidth < 2282) {}else {
     lifeCycles.addClass('small');
+    aquatics.addClass('small');
   }
 });

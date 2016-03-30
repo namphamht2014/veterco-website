549edf2e72194e4d9d21cd6cf996db0f6b064edc
nam pham <nam@vnasoft.com>
2016-03-30 16:07:20 +0700

    Update xong phan Product Forms

--- application/views/frontend/sourcing_product_forms.php
+++ application/views/frontend/sourcing_product_forms.php
@@ -8,16 +8,27 @@
   </div>
   <div class="content">
     <div class="cycle">
-      <img src="/assets/images/productForms-1.png" alt="" class="form1"/><span class="text1"><i><u>Powder</u></i></span>
-      <img src="/assets/images/productForms-2.png" alt="" class="form2"/><span class="text2"><i><u>Ointment</u></i></span>
-      <img src="/assets/images/productForms-3.png" alt="" class="form3"/><span class="text3"><i><u>Liquid</u></i></span><span class="text3_2"><i><u>Solution</u></i></span>
-      <img src="/assets/images/productForms-4.png" alt="" class="form4"/><span class="text4"><i><u>Lick block</u></i></span>
+      <img src="/assets/images/productForms-1.png" alt="" class="form1"/>
+      <img src="/assets/images/productForms-1-active.png" alt="" class="form1-active"/>
+      <span class="text1"><i><u>Powder</u></i></span>
+
+      <img src="/assets/images/productForms-2.png" alt="" class="form2"/>
+      <img src="/assets/images/productForms-2-active.png" alt="" class="form2-active"/>
+      <span class="text2"><i><u>Ointment</u></i></span>
+
+      <img src="/assets/images/productForms-3.png" alt="" class="form3"/>
+      <img src="/assets/images/productForms-3-active.png" alt="" class="form3-active"/>
+      <span class="text3"><i><u>Liquid</u></i></span><span class="text3 text3_2"><i><u>Solution</u></i></span>
+
+      <img src="/assets/images/productForms-4.png" alt="" class="form4"/>
+      <img src="/assets/images/productForms-4-active.png" alt="" class="form4-active"/>
+      <span class="text4"><i><u>Lick block</u></i></span>
 
       <div class="controls">
         <a><div class="container ctop"><div class="showBlock1 active"></div></div></a>
         <a><div class="container cright"><div class="showBlock2"></div></div></a>
-        <a><div class="container cleft"><div class="showBlock3"></div></div></a>
-        <a><div class="container cbottom"><div class="showBlock4"></div></div></a>
+        <a><div class="container cbottom"><div class="showBlock3"></div></div></a>
+        <a><div class="container cleft"><div class="showBlock4"></div></div></a>
       </div>
     </div>
   </div>
--- assets/js/fsourcing_productForm.js
+++ assets/js/fsourcing_productForm.js
@@ -1,28 +1,66 @@
 $(function () {
-  $('.productFormsContainer .controls .container').mouseenter(function () {
+  var order = 1;
+  var isPaused = false;
+  setInterval(function() {
+    if (!isPaused) {
+      order++;
+
+      $('.productFormsContainer .cycle img').hide();
+      $('.productFormsContainer .cycle span').hide();
+      $('.productFormsContainer .controls .container div').removeClass('active');
+
+      $('.productFormsContainer .cycle img.form'+order).show();
+      $('.productFormsContainer .cycle span.text'+order).show();
+      $('.productFormsContainer .controls .container div.showBlock'+order).addClass('active');
+
+      if (order >= 4) order = 0;
+    }
+
+  }, 3000);
+
+  $('.productFormsContainer .cycle img').mouseenter(function () {
+    isPaused = true;
+
+    if ($(this).hasClass('form1') ||
+        $(this).hasClass('form2') ||
+        $(this).hasClass('form3') ||
+        $(this).hasClass('form4')) {
+      $(this).hide();
+      var tmpClass = $(this).attr('class');
+      $('.'+tmpClass+'-active').show();
+    }
+  });
+  $('.productFormsContainer .cycle img').mouseleave(function () {
+
+    if ($(this).hasClass('form1-active') ||
+        $(this).hasClass('form2-active') ||
+        $(this).hasClass('form3-active') ||
+        $(this).hasClass('form4-active')) {
+      isPaused = false;
+    }
+  });
+  $('.productFormsContainer .controls .container div').mouseenter(function () {
+    isPaused = true;
     $('.productFormsContainer .controls .container div').removeClass('active');
+    $(this).addClass('active');
     $('.productFormsContainer .cycle img').hide();
     $('.productFormsContainer .cycle span').hide();
 
-    if ($(this).hasClass('ctop')) {
-      $('.showBlock1').addClass('active');
-      $('.productFormsContainer .cycle .form1').show();
+    if ($(this).hasClass('showBlock1')) {
+      order = 1;
+      $('.productFormsContainer .cycle img.form1-active').show();
       $('.productFormsContainer .cycle span.text1').show();
-    }
-    if ($(this).hasClass('cright')) {
-      $('.showBlock2').addClass('active');
-      $('.productFormsContainer .cycle .form2').show();
+    }else if ($(this).hasClass('showBlock2')) {
+      order = 2;
+      $('.productFormsContainer .cycle img.form2-active').show();
       $('.productFormsContainer .cycle span.text2').show();
-    }
-    if ($(this).hasClass('cleft')) {
-      $('.showBlock3').addClass('active');
-      $('.productFormsContainer .cycle .form3').show();
+    }else if ($(this).hasClass('showBlock3')) {
+      order = 3;
+      $('.productFormsContainer .cycle img.form3-active').show();
       $('.productFormsContainer .cycle span.text3').show();
-      $('.productFormsContainer .cycle span.text3_2').show();
-    }
-    if ($(this).hasClass('cbottom')) {
-      $('.showBlock4').addClass('active');
-      $('.productFormsContainer .cycle .form4').show();
+    }else if ($(this).hasClass('showBlock4')) {
+      order = 0;
+      $('.productFormsContainer .cycle img.form4-active').show();
       $('.productFormsContainer .cycle span.text4').show();
     }
   });
